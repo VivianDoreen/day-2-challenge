27989754cdc29c0a55184c6429162b72fcb401f5
Nabulo Vivian <nabulovivian2014@gmail.com>
2018-09-05 15:41:01 -0700

    [finishes #160304614] add comments

--- fizzbuz.py
+++ fizzbuz.py
@@ -8,17 +8,17 @@ def fizzbuzz(list1, list2):
     length_list1 = len(list1)
     length_list2 = len(list2)
     combined_length = length_list1 + length_list2
-    #if combined list length is divisible by 3
     divisible_by_3 = combined_length%3
     divisible_by_5 = combined_length%5
     divisible_by_combined_length = combined_length%combined_length
-    if divisible_by_3 == 0:
+    
+    if divisible_by_3 == 0:#if combined list length is divisible by 3
         print("fizz")
         return "fizz"
-    elif divisible_by_5 == 0:
+    elif divisible_by_5 == 0:#if combined list length is divisible by 5
         print("buzz")
         return "buzz"
-    elif divisible_by_3 == 0 and divisible_by_5 == 0 or divisible_by_combined_length == 0:
+    elif divisible_by_3 == 0 and divisible_by_5 == 0 or divisible_by_combined_length == 0:#if combined list length is divisible by 3 and 5 or the the combined length of the list
         print("fizzbuzz")
         return "fizzbuzz"
     else:
