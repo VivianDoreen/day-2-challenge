868d2c4224c2177169ea23301bfdd2748212f55a
Nabulo Vivian <nabulovivian2014@gmail.com>
2018-09-05 15:35:13 -0700

    [finishes #160304614] a function that returnd fizz, buzz or fizzbuzz

---
+++ fizzbuz.py
@@ -0,0 +1,29 @@
+"""
+a function that returns Fizz if the combined length of the lists is
+divisible by 3,  Buzz if it is divisible by 5, Fizzbuzz if it is divisible
+by both 5 and 3  or the combined length of the list
+"""
+def fizzbuzz(list1, list2):
+    "method that returns Fizz, Buzz and FizzBuzz"
+    length_list1 = len(list1)
+    length_list2 = len(list2)
+    combined_length = length_list1 + length_list2
+    #if combined list length is divisible by 3
+    divisible_by_3 = combined_length%3
+    divisible_by_5 = combined_length%5
+    divisible_by_combined_length = combined_length%combined_length
+    if divisible_by_3 == 0:
+        print("fizz")
+        return "fizz"
+    elif divisible_by_5 == 0:
+        print("buzz")
+        return "buzz"
+    elif divisible_by_3 == 0 and divisible_by_5 == 0 or divisible_by_combined_length == 0:
+        print("fizzbuzz")
+        return "fizzbuzz"
+    else:
+        print("Not divisible by any")
+
+fizzbuzz([1, 2, 3], [ ])    # will return “fizz”
+fizzbuzz([1, 2, 3], [1, 2])  # will return “buzz”  
+fizzbuzz([1, 2, 3], [1]) # will return 4 
